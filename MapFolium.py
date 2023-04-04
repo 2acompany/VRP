b19ae2d37aa3b007811abcdcfbd40da34ec18982
Behrang Aghilinasab <behrang.aghili@gmail.com>
2023-04-04 22:45:34 +0430

    update

--- MapFolium.py
+++ MapFolium.py
@@ -27,15 +27,15 @@ def generate_random_nodes_on_iran_border(num_nodes):
 
 def map():
     # Define the latitude and longitude bounds for Tehran city limits
-    #tehran_limits = [(35.4904, 51.1424), (35.8496, 51.6814)]
-    tehran_limits = [(25.0644, 44.0328), (39.7828, 63.3167)]
+    tehran_limits = [(35.4904, 51.1424), (35.8496, 51.6814)]
+    #tehran_limits = [(25.0644, 44.0328), (39.7828, 63.3167)]
 
     # Number of nodes
     num_nodes = random.randint(50, 50)
 
     # Generate random latitude and longitude coordinates for each node within Tehran city limits
-    #coords = [(random.uniform(tehran_limits[0][0], tehran_limits[1][0]), random.uniform(tehran_limits[0][1], tehran_limits[1][1])) for _ in range(num_nodes)]
-    coords = generate_random_nodes_on_iran_border(num_nodes)           
+    coords = [(random.uniform(tehran_limits[0][0], tehran_limits[1][0]), random.uniform(tehran_limits[0][1], tehran_limits[1][1])) for _ in range(num_nodes)]
+    #coords = generate_random_nodes_on_iran_border(num_nodes)           
 
     # Calculate the distance matrix between the nodes
     dist_matrix = [[0] * num_nodes for _ in range(num_nodes)]
@@ -64,7 +64,7 @@ def map():
         routes: _description_
     """
     #Get routes from vrp solution
-    routes= main(dist_matrix, 50, 0)
+    routes= main(dist_matrix, 20, 0)
     
     #diffrent random color for each poly line and marker
     for route in routes:
@@ -73,8 +73,8 @@ def map():
             start_node = route[i]
             end_node = route[i+1]
             folium.PolyLine(locations=[coords[start_node], coords[end_node]], color=color).add_to(m)
-            folium.Marker(location=coords[start_node], icon=folium.Icon(icon='pushpin', color='blue',tooltip=f'Node {i}' )).add_to(m)
-            #folium.Circle( radius=100,location=coords[start_node], popup="The Waterfront",color=color, fill=False, ).add_to(m)
+            #folium.Marker(location=coords[start_node], icon=folium.Icon(icon='pushpin', color='blue',tooltip=f'Node {i}' )).add_to(m)
+            folium.Circle( radius=100,location=coords[start_node], popup="The Waterfront",color=color, fill=False, ).add_to(m)
            
     # Add markers for the Depot
     #for i in range(1):
